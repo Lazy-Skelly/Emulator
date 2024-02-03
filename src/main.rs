0ef44559fa65b288c07c5dedfb0fa4c3ab458166
HumbleFriedToast <dalilfaycal2004@gmail.com>
2024-02-03 20:13:30 +0000

    changes 2 electric boogalo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+
+
+
 
 enum Adressing_mode{
     Immediate,
@@ -19,34 +25,65 @@ pub struct Cpu{
     pub memory : [u8 ; 0xFFFF],
 }
 
+
+
+
+
+
+
+
+
+
 impl Cpu{
     pub fn new() -> Self { 
         Cpu {
             reg_a : 0,
             reg_x : 0,
             reg_y : 0,
-            pc : 0,
-            status : 0,
+            pc : 0b00000000,
+            status : 0b00100000,
             memory : [0 ; 0xFFFF],
 
+        }}
+    pub fn BRK(&mut self){
+            //push(self.pc); push(self.status);
+            self.status = self.status | 0b00010000;
+            self.pc = 0x0000 | (self.memory[0xFFFE] as u16);
+            self.pc = self.pc | ((self.memory[0xFFFE] as u16)*256);            
+        
         }
+    pub fn INX(&mut self){ 
+        if self.reg_x == 0b11111111{
+            self.status = self.status | 0b00000010;
+        }
+        self.status += 1; 
+        if (self.status  & 0b10000000 == 0b10000000 ){
+            self.status = self.status | 0b10000000;
+        }
+         
+        
+
+
     }
+    
 }
+
 
 struct opcode{
     pub name : String,
     pub code : u8,
     pub length : u8,
     pub cycle : u8,
-    mode : Adressing_mode,
+    pub mode : Adressing_mode,
 }
 impl opcode{
     fn new(s : String, c : u8,l : u8, cy : u8 , a : Adressing_mode) -> Self {
         opcode { name: (s), code: (c), length: (l), cycle: (cy), mode: (a) }
     }
 }
-/*tp do:
-        Flag modifiers
+/*  ToDo:
+        Create Stack {$0100-$0200}
+
  */
 
 
