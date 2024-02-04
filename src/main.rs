d1683bfe97ad602bb9a74d23b8ddccf50cde15ae
Lazy-Skelly <133169650+Lazy-Skelly@users.noreply.github.com>
2024-02-04 22:37:37 +0100

    added memory functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+
 pub enum Adressing_mode{
     Immediate,
     Zeropage,
@@ -16,23 +19,93 @@ pub struct Cpu{
     pub reg_y : u8,
     pub pc : u16,
     pub status : u8,
-    pub memory : [u8 ; 0xFFFF],
+    pub memory : [u8 ; 0x10000],
 }
 
+
+#[allow(non_snake_case)]
 impl Cpu{
     pub fn new() -> Self { 
         Cpu {
             reg_a : 0,
             reg_x : 0,
             reg_y : 0,
-            pc : 0,
+            pc : 0b00000000,
             status : 0b00100000,
-            memory : [0 ; 0xFFFF],
+            memory : [0 ; 0x10000],
 
-        }
+        }}
+
+    pub fn Read_memory(&mut self, adress:u16) -> u8{
+        self.memory[adress as usize]
     }
     
-    pub fn lda(&mut self, mode :Adressing_mode){
+    pub fn Write_memory(&mut self, adress:u16, data:u8){
+        self.memory[adress as usize] = data;
+    }
+    
+    pub fn Read_memory_16(&mut self, adress:u16) ->u16{
+        let low :u16= self.Read_memory(adress) as u16;
+        let high : u16= self.Read_memory(adress +1) as u16;
+        (high << 8) | low
+    }
+    
+    pub fn Write_memory_16(&mut self, adress:u16, data:u16){
+        let high = (data >> 8) as u8;
+        let low = (data & 0xff) as u8;
+        self.Write_memory(adress, low);
+        self.Write_memory(adress+1, high);
+    }
+     
+    pub fn Get_operand_adress(&mut self, mode :Adressing_mode) -> u16{
+        match mode{
+            Adressing_mode::Immediate => self.pc,
+            Adressing_mode::Zeropage => self.Read_memory(self.pc) as u16,
+            Adressing_mode::Zeropage_X => ((self.Read_memory(self.pc)+self.reg_x)&0xff) as u16,
+            Adressing_mode::Zeropage_Y => ((self.Read_memory(self.pc)+self.reg_y)&0xff) as u16,
+            Adressing_mode::Absolute => self.Read_memory_16(self.pc),
+            Adressing_mode::Absolute_X => self.Read_memory_16(self.pc) + self.reg_x as u16,
+            Adressing_mode::Absolute_Y => self.Read_memory_16(self.pc) + self.reg_y as u16,
+            Adressing_mode::Indirect_X  => {
+                let adress = ((self.Read_memory(self.pc)+self.reg_x)&0xff) as u16;
+                let low = self.Read_memory(adress) as u16;
+                let high = self.Read_memory((adress +1)&0xff) as u16;
+                high << 8 | low
+            },
+            Adressing_mode::Indirect_Y => {
+                let adress = self.Read_memory(self.pc) as u16;
+                let low = self.Read_memory(adress) as u16;
+                let high = self.Read_memory((adress+1)&0xff) as u16;
+                let adress = high << 8 | low;
+                adress + self.reg_y as u16
+            },
+        }
+    } 
+     
+    pub fn BRK(&mut self){
+            //push(self.pc); push(self.status);
+            self.status = self.status | 0b00010000;
+            self.pc = 0x0000 | (self.memory[0xFFFE] as u16);
+            self.pc = self.pc | ((self.memory[0xFFFE] as u16)*256);            
+        
+        }
+    pub fn INX(&mut self){        
+        let x = self.reg_x;
+        if x == 0b11111111 {
+            self.Set_zero_flag(true);
+            self.Set_negative_flag(false);
+            self.reg_x = 0b00000000;
+        }else{
+            self.reg_x += 1; 
+            if 0b10000000 == 0b10000000 & self.reg_x {
+                self.Set_negative_flag(true);
+            }else{
+                self.Set_negative_flag(false);
+            }
+        }         
+    }
+
+    pub fn LDA(&mut self, mode :Adressing_mode){
         match mode {
             Adressing_mode::Immediate => {
                 let x = self.memory[self.pc as usize];
@@ -40,7 +113,7 @@ impl Cpu{
                 if x == 0 {
                     self.Set_zero_flag(true);
                 }else{
-                    self.Set_negative_flag(false);
+                    self.Set_zero_flag(false);
                 }
                 if 0b10000000 == 0b10000000 & x {
                     self.Set_negative_flag(true);
@@ -54,13 +127,13 @@ impl Cpu{
         }
     }
     
-    pub fn tax(&mut self){
+    pub fn TAX(&mut self){
         self.reg_a = self.reg_x;
         let x = self.reg_a;
         if x == 0 {
             self.Set_zero_flag(true);
         }else{
-            self.Set_negative_flag(false);
+            self.Set_zero_flag(false);
         }
         if 0b10000000 == 0b10000000 & x {
             self.Set_negative_flag(true);
@@ -126,26 +199,31 @@ impl Cpu{
     }
     
 }
+
 #[allow(non_camel_case_types)]
 struct opcode{
     pub name : String,
     pub code : u8,
     pub length : u8,
     pub cycle : u8,
-    mode : Adressing_mode,
+    pub mode : Adressing_mode,
 }
 impl opcode{
     fn new(s : String, c : u8,l : u8, cy : u8 , a : Adressing_mode) -> Self {
         opcode { name: (s), code: (c), length: (l), cycle: (cy), mode: (a) }
     }
 }
-/*tp do:
-        Flag modifiers
+/*  ToDo:
+        Create Stack {$0100-$0200}
+
  */
 
-
-
-
 fn main() {
-
+    let mut c = Cpu::new();
+    c.Write_memory(0,5);
+    c.Write_memory(1,5);
+    println!("{}",c.Read_memory_16(0));
+    c.Write_memory_16(1,0x0000);
+    println!("{}",c.Read_memory(2));
+    println!("{}",c.Get_operand_adress())
 }
